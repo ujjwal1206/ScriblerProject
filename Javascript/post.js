9c0db128a247259de418c209bcc468dd09faaa1b
ujjwal1206 <ujjwal1985.cse18@chitkara.edu.in>
2021-03-27 16:45:42 +0530

    maximum part is done

---
+++ Javascript/post.js
@@ -0,0 +1,75 @@
+var editable = false;
+var a = 1;
+var str = new Array();
+
+
+function Edit() {
+    a = a + 1;
+    var eh = document.getElementById("editableheading");
+    var ec = document.getElementById("editablecontent");
+    if (!editable) {
+        eh.style.border = '1px';
+        eh.style.borderStyle = 'solid';
+
+        ec.style.border = '1px';
+        ec.style.borderStyle = 'solid';
+
+        document.getElementById("editbtn").innerHTML = "Save <i class=\"glyphicon glyphicon-floppy-save\"></i>";
+
+        editable = true;
+    } else {
+
+        if (a == 2) {
+            var t = document.getElementById("editableheading");
+            eh.innerHTML = t.innerHTML;
+
+            var temp = document.getElementById("editablecontent");
+            ec.innerHTML = temp.innerHTML;
+
+        }
+
+        eh.style.border = 'none';
+        ec.style.border = 'none';
+
+        document.getElementById("editbtn").innerHTML = "Edit <i class=\"glyphicon glyphicon-edit\"></i>";
+
+    }
+}
+window.onload = function() {
+
+    if (str.length == 0) {
+        if (window.location.search.split('?').length > 1) {
+            const par = window.location.search.split('?')[1].split('&');
+            for (let i = 0; i < par.length; i++) {
+                const k = par[i].split('=')[0];
+                const v = decodeURIComponent(par[i].split('=')[1]);
+                str[k] = v;
+            }
+        }
+    }
+
+    if (str.h != null && str.writer != null) {
+        const { h } = str;
+        const { writer } = str;
+        const { c } = str;
+        document.getElementsByClassName('heading-content')[0].innerHTML = h;
+        document.getElementsByClassName('writername')[0].innerHTML = writer;
+        document.getElementsByClassName('post-content')[0].innerHTML = c;
+    }
+
+    document.getElementById('comment').style.visibility = 'hidden';
+}
+
+var c = 0;
+
+function count1() {
+
+    c = c + 1;
+    document.getElementById("likebtn").innerHTML = "Liked";
+    if (c == 1) {
+        document.getElementById("counter").innerHTML = c + " person liked this !";
+    } else {
+        document.getElementById("counter").innerHTML = c + " people liked this !";
+    }
+
+}
--- Javascript/postlist.js
+++ Javascript/postlist.js
@@ -1,11 +1,5 @@
-function remove(post) {
-    const del = document.getElementById(post);
-    del.parentNode.removeChild(delCard);
-}
+function open1(writer, h, c) {
+    const url = `../html/post.html?h=${encodeURIComponent(h.innerText)}&writer=${encodeURIComponent(writer.innerText)}&c=${encodeURIComponent(c.innerText)}`;
+    window.location.href = url;
 
-// function openPost(author, heading, content) {
-//     const url = `../html/post.html?heading=${encodeURIComponent(heading.innerText)}&author=${encodeURIComponent(
-//             author.innerText
-//     )}&content=${encodeURIComponent(content.innerText)}`;
-//     window.location.href = url;
-// }
+}
