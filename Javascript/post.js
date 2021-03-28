b9d107c6dd7878a35ba77c9737fb5e246f60ec7c
ujjwal1206 <ujjwal1985.cse18@chitkara.edu.in>
2021-03-28 12:40:49 +0530

    final commit

--- Javascript/post.js
+++ Javascript/post.js
@@ -2,14 +2,39 @@ var editable = false;
 var a = 1;
 var str = new Array();
 
+window.onload = function() {
+
+    if (str.length == 0) {
+        if (window.location.search.split('?').length > 1) {
+            const par = window.location.search.split('?')[1].split('&');
+            for (let i = 0; i < par.length; i++) {
+                const k = par[i].split('=')[0];
+                const v = decodeURIComponent(par[i].split('=')[1]);
+                str[k] = v;
+            }
+        }
+    }
+
+    if (str.h != null && str.writer != null) {
+        const { h } = str;
+        const { writer } = str;
+        const { c } = str;
+        document.getElementsByClassName('heading-content')[0].innerHTML = h;
+        document.getElementsByClassName('writername')[0].innerHTML = writer;
+        document.getElementsByClassName('post-content')[0].innerHTML = c;
+    }
+}
 
 function Edit() {
     a = a + 1;
     var eh = document.getElementById("editableheading");
     var ec = document.getElementById("editablecontent");
+    eh.contentEditable = "true";
+    ec.contentEditable = "true";
     if (!editable) {
         eh.style.border = '1px';
         eh.style.borderStyle = 'solid';
+        eh.style.borderColor = 'red';
 
         ec.style.border = '1px';
         ec.style.borderStyle = 'solid';
@@ -35,30 +60,6 @@ function Edit() {
 
     }
 }
-window.onload = function() {
-
-    if (str.length == 0) {
-        if (window.location.search.split('?').length > 1) {
-            const par = window.location.search.split('?')[1].split('&');
-            for (let i = 0; i < par.length; i++) {
-                const k = par[i].split('=')[0];
-                const v = decodeURIComponent(par[i].split('=')[1]);
-                str[k] = v;
-            }
-        }
-    }
-
-    if (str.h != null && str.writer != null) {
-        const { h } = str;
-        const { writer } = str;
-        const { c } = str;
-        document.getElementsByClassName('heading-content')[0].innerHTML = h;
-        document.getElementsByClassName('writername')[0].innerHTML = writer;
-        document.getElementsByClassName('post-content')[0].innerHTML = c;
-    }
-
-    document.getElementById('comment').style.visibility = 'hidden';
-}
 
 var c = 0;
 
@@ -77,7 +78,8 @@ function count1() {
 function Coment() {
     var comment = document.getElementById("comments");
     if (comment.value != '') {
-        document.getElementById("c2").innerHTML += "<div id=\"c1\">" + comment.value + "</div>";
+        document.getElementsByClassName("comment-container")[0].innerHTML += "<div id=\"c1\">" + comment.value + "</div>";
         comment.value = "";
     }
+
 }
